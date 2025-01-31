9c528265e4e349695004912bf6d6d4531d6cc666
Your Name <you@example.com>
2025-01-31 20:51:21 -0300

    first commit

---
+++ apontamento1.py
@@ -0,0 +1,57 @@
+# Variáveis: são caixinhas que armazenam toda informação que colocamos no código.
+
+# No Python não existe Var, Let e Const. Para criarmos uma variável, precisamos apenas do nome e a informação que essa variável vai receber.
+
+# Exemplo:
+
+prato_da_semana = "picanha ao molho gorgonzola com fritas"
+
+prato_da_semana = "escondidinho de macaxeira com carne de sol"
+
+prato_da_semana = 3.14
+
+# Dessa maneira toda vez que uma variável for substituída, a última alteração é que permanecerá.
+
+# O uso do underline chama-se Snake_Case (Python).
+
+# Já no Javascript utiliza-se o CamelCase. Exemplo: pratoDaSemana = 3.14
+
+# Tipos de Dados que existem em Python:
+
+# Inteiros (Int) -> Todos os números positivos ou negativos que não possuem vírgula ou ponto.
+
+# Exemplo:
+preco_escondidinho = 20
+divida_fernanda = -100
+
+# Ponto Flutuante ou Decimal (float) -> Todos os números positivos ou negativos que possuem vírgula ou ponto.
+
+# Exemplo:
+preco_picanha = 39,90
+pi = -3.14
+
+# Strings ou Caracteres (str) -> São considerados todos os tipos de Dados cujo entre entre está "" ou ''
+
+# Exemplo:
+prato_dia = "lasanha de camarão com catupiry com 3 acompanhamentos"
+prato_dia = 'lasanha de camarão com catupiry com 3 acompanhamentos'
+#numero_da_sorte = "7"  ("" e '' diferenciam-se pela tecla Shift)
+
+# Booleanos (bool) -> São todas as informações que possuem valor True e False (ambos escritos comiicial maiúscula)
+
+# Exemplo:
+maior_de_idade = True
+conta_tem_saldo = False
+
+# Nulos (None) -> São informações do tipo Nula (None)
+
+# Exemplo: 
+valor_nulo = None
+
+
+# type(): funcionalidade que verifica o tipo da variável. Exibe na tela o tipo da variável
+
+print(type(maior_de_idade))
+
+
+
